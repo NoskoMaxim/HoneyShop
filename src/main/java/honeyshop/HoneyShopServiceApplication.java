1054b68b3b6a7d22d597937a1d78d5a7501c19c1
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-02-11 20:56:54 +0300

    ADDED pom.xml file, HoneyShop service application, application properties

---
+++ src/main/java/honeyshop/HoneyShopServiceApplication.java
@@ -0,0 +1,19 @@
+package honeyshop;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HoneyShopServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HoneyShopServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> System.out.println("Honey Shop is active and ready to go!");
+    }
+}
