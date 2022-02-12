7705dac7c14602eaef3e6fc84b5eaf2e9d6c5c64
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-02-12 22:14:52 +0300

    ADDED Docker Configuration, database CONECTED succes

--- src/main/java/honeyshop/HoneyShopServiceApplication.java
+++ src/main/java/honeyshop/HoneyShopServiceApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class HoneyShopServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(HoneyShopServiceApplication.class, args);
