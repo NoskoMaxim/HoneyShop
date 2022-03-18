f2b6690ad5e872738369ca3e3926ddbbfc7e45a2
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-03-18 22:29:05 +0300

    REFACTORED Imports

--- src/main/java/honeyshop/model/productbeekeeping/ProductBeekeeper.java
+++ src/main/java/honeyshop/model/productbeekeeping/ProductBeekeeper.java
@@ -1,6 +1,11 @@
 package honeyshop.model.productbeekeeping;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
