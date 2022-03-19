88419da10ed1d12dd7cbf3e201575fe685bc58a2
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-03-19 19:31:14 +0300

    AUTOWIREDED Product Beekeeper Adapter class

--- src/main/java/honeyshop/service/productbeekeeping/ProductBeekeeperServiceImpl.java
+++ src/main/java/honeyshop/service/productbeekeeping/ProductBeekeeperServiceImpl.java
@@ -1,29 +1,33 @@
 package honeyshop.service.productbeekeeping;
 
-import honeyshop.adapter.productbeekeeping.*;
+import honeyshop.adapter.productbeekeeping.ProductBeekeeperAdapter;
 import honeyshop.config.exception.honeyshopexception.HoneyShopException;
 import honeyshop.dto.productbeekeeping.ProductBeekeeperDto;
 import honeyshop.model.productbeekeeping.ProductBeekeeper;
 import honeyshop.repository.section.ProductBeekeeperRepos;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.*;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.GONE;
 
 @Service
 @Transactional
 public class ProductBeekeeperServiceImpl implements ProductBeekeeperService {
 
     private final ProductBeekeeperRepos productBeekeeperRepos;
-    private final ProductBeekeeperAdapter productBeekeeperAdapter = new ProductBeekeeperAdapterImpl();
+    private final ProductBeekeeperAdapter productBeekeeperAdapter;
 
     @Autowired
-    public ProductBeekeeperServiceImpl(ProductBeekeeperRepos productBeekeeperRepos) {
+    public ProductBeekeeperServiceImpl(ProductBeekeeperRepos productBeekeeperRepos, ProductBeekeeperAdapter productBeekeeperAdapter) {
         this.productBeekeeperRepos = productBeekeeperRepos;
+        this.productBeekeeperAdapter = productBeekeeperAdapter;
     }
 
     @Override
